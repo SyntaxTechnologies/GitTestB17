c90f34be359b7d17dd9ba5050c0afd7f62864000
Syntax <instructor@syntaxtechs.com>
2023-10-31 20:55:11 -0400

    Update basicCode.java

--- src/test/java/basicCode.java
+++ src/test/java/basicCode.java
@@ -3,5 +3,6 @@ import java.sql.SQLOutput;
 public class basicCode {
     public static void main(String[] args) {
         System.out.println("hello B17 ");
+  System.out.println("Abracdbraaa  ");
     }
 }
